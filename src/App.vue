ff3e0b685b86116544bb6f59ea937112fdd9f114
marinagarces <31573683+marinagarces@users.noreply.github.com>
2021-10-21 22:14:43 -0300

    commit inicial y final

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="titulostyle">
+      <h1>{{ mensaje }} </h1>
+      Titulo:<input v-model="titulo"> <br/><br/>
+    </div>
+
+    <div class="cuerpostyle">
+      <label for="filtro">Filtro:</label>
+      <select name="filtro" id="filtro" v-model="filtroseleccionado">
+        <option v-for="(tipo,index) in filtro" v-bind:key="index" v-bind:value="tipo.valor">{{ tipo.texto }}</option>
+        </select><br/><br/>
+        <label for="colortexto">Color:</label>
+      <select class="inlinee" name="colortexto" id="colortexto" v-model="colorseleccionado">
+        <option v-for="(elcolor,index) in colortexto" v-bind:key="index" v-bind:value="elcolor.valor">{{ elcolor.texto }}</option>
+        </select><div class="circulo" v-bind:style="{'background-color':colorseleccionado}"></div><br/><br/>
+        Tamaño:<input v-model="tamano">  <br/><br/>
+    </div>
+
+    <div class="btnstyle">
+          <button v-on:click="enviaralhijo">Obtener mi gatito</button><br/>
+        <Randomcat v-bind:eltitulo="tituloaenviar" v-bind:elfiltro="filtroaenviar" v-bind:elcolor="coloraenviar" v-bind:eltamano="tamanoaenviar"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Randomcat from './components/Randomcat.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Randomcat
+  },
+  data(){
+    return{
+      mensaje: 'Random Gif Cat',
+      titulo:'',
+      tituloaenviar:'',
+      filtro:[
+      {texto:'Blur', valor:'blur',},
+      {texto:'Mono', valor:'mono',},
+      {texto:'Sepia', valor:'sepia',},
+      {texto:'Negative', valor:'negative',},
+      {texto:'Paint', valor:'paint',},
+      {texto:'Pixel', valor:'pixel',},
+      ],
+      filtroseleccionado: '',
+      filtroaenviar: '',
+      colortexto:[
+      {texto:'Rojo', valor:'red',},
+      {texto:'Verde', valor:'green',},
+      {texto:'Blue', valor:'blue',},
+      {texto:'Amarillo', valor:'yellow',},
+      {texto:'Purpura', valor:'purple',},
+      {texto:'Rosado', valor:'pink',},
+      ],
+      colorseleccionado: '',
+      coloraenviar: '',
+      tamano:'',
+      tamanoaenviar: '',
+    }
+  },
+  methods:{
+    enviaralhijo: function(){
+      this.tituloaenviar = this.titulo;
+      this.filtroaenviar = this.filtroseleccionado;
+      this.coloraenviar = this.colorseleccionado;
+      this.tamanoaenviar = this.tamano;
+
+      setTimeout(()=>{
+        this.tituloaenviar = '';
+        this.titulo = '';
+        this.filtroaenviar = '';
+        this.filtroseleccionado = '';
+
+      }, 1000);
+
+
+    },
+  },
 }
 </script>
 
@@ -23,6 +89,30 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
+
+}
+.titulostyle, .btnstyle {
+background-color: salmon;
+padding: 20px;
+}
+.cuerpostyle {
+background-color: #1FD8DF;
+padding: 20px;
+}
+.circulo {
+    width: 20px;
+    height: 20px;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+    background: white;
+    position: absolute;
+    display: inline;
+    margin-left: 5px;
+}
+.inlinee {
+position: relative;
+
 }
 </style>
---
+++ src/components/Randomcat.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="Randomcat">
+  <p>{{ escucharprops }}</p>
+  <p>{{ elfiltro }}</p>
+  <p>{{ elcolor }}</p>
+  <p>{{ eltamano }}</p>
+  <div>
+    <img v-bind:src="respuestaapi" >
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Randomcat',
+  props: {
+    eltitulo: String,
+    elfiltro: String,
+    elcolor: String,
+    eltamano: String,
+  },
+  data(){
+    return{
+      respuestaapi:'',
+    }
+  },
+  computed: {
+    escucharprops:function(){
+      if(!this.eltitulo==''){
+        this.consumirapigatos();
+      }
+      return this.eltitulo;
+    }
+  },
+  methods:{
+    consumirapigatos(){
+      this.respuestaapi = 'Cargando...';
+      fetch('https://cataas.com/cat/gif')
+        .then(response=> {console.log(response)
+          this.respuestaapi = response.url});
+
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Randomcat{
+
+}
+
+</style>
